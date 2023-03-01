f6d15ac3b9527d2bed1eb9e9e3aedf641e1b187c
Arun Yadav <engg.aruny@gmail.com>
2023-03-02 00:52:25 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using dotnet8_playground;
+using System.Text.Json;
+
+//Support for serializing properties from interface hierarchies.
+IEmployee employeeObj = new Employee { MaxAge = 35, SalaryRevisionDate = DateTimeOffset.Now };
+var jsonString = JsonSerializer.Serialize(employeeObj);
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Support for serializing properties from interface hierarchies\n");
+Console.WriteLine(jsonString + "\n");
+
+//JsonNamingPolicy includes new naming policies for snake_case (with an underscore)
+var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower };
+var jsonSnakeString = JsonSerializer.Serialize(employeeObj, options);
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("JsonNamingPolicy includes new naming policies for snake_case (with an underscore)\n");
+Console.WriteLine(jsonSnakeString + "\n");
+
+
+//Testing Randomness
+Console.WriteLine("Testing Randomness\n");
+RandomnessDemo.TestRandomness();
+
+Console.ResetColor();
+public interface IPerson
+{
+    public int MaxAge { get; set; }
+}
+
+public interface IEmployee : IPerson
+{
+    public DateTimeOffset SalaryRevisionDate { get; set; }
+}
+
+public class Employee : IEmployee
+{
+    public int MaxAge { get; set; }
+    public DateTimeOffset SalaryRevisionDate { get; set; }
+}
---
+++ RandomnessDemo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet8_playground
+{
+    public static class RandomnessDemo
+    {
+        private static readonly string[] s_data = { "Red", "Green", "Blue" };
+
+        public static void TestRandomness()
+        {
+            //Get Item
+            string[] result = Random.Shared.GetItems<string>(s_data, 31);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Get Item\n");
+            Console.WriteLine("[{0}]\n", string.Join(", ", result));
+
+            //Shuffle
+            string[] trainingData = LoadTrainingData();
+            Random.Shared.Shuffle(trainingData);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Shuffle\n");
+            Console.WriteLine("[{0}]\n", string.Join(", ", s_data));
+        }
+
+        public static string[] LoadTrainingData()
+        {
+            return s_data;
+        }
+    }
+}
